dde5eee5260b2b4ae4cff5a64e1ef9664e16249d
flquinnn <13serendipity22@gmail.com>
2026-05-04 17:05:31 +0700

    feat: complete Exercise 10 - Drag & Drop

---
+++ Xpath-Assignments/src/main/java/org/example/base/BaseTest.java
@@ -0,0 +1,29 @@
+package org.example.base;
+
+import com.microsoft.playwright.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected Playwright playwright;
+    protected Browser browser;
+    protected BrowserContext context;
+    protected Page page;
+
+    @BeforeMethod
+    public void setup() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                .setHeadless(false)
+                .setSlowMo(800));
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (context != null) context.close();
+        if (browser != null) browser.close();
+        if (playwright != null) playwright.close();
+    }
+}
